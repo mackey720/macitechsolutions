a0e7b6ba979404eeff2c3d4cb2e5e922f2d314ba
Alicia McPherson <mackey720@gmail.com>
2021-06-16 19:23:19 -0400

    update

--- MacitechSolutions/settings.py
+++ MacitechSolutions/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
+
+#reading .env file
+env = environ.Env()
 
 from pathlib import Path
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-il7=tw+c)!7%811t-q#+xet7pofj0t1kuwh3=1rxzz0n*y$b@e'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
